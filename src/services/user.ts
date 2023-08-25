5faf4710f6a3280954d41b839720e8fbc7fedef1
pranav603081 <manikandan.murugesan@aspiresys.com>
2023-08-25 15:09:03 +0530

    service class add

---
+++ src/services/user.ts
@@ -0,0 +1,13 @@
+import { userRepository } from '../repositories';
+
+class UserService {
+  /**
+   *
+   * @param userDetails - user details from token
+   * @returns - list of user owned accounts and opportunities
+   */
+  async getOwnedAccounts(userDetails) {}
+
+}
+
+export const userService = new UserService();
